d962456ef5df10b0d058f2027f02753d16825bdd
Felipe Brahm <fbrahm@groupon.com>
2013-10-03 19:35:53 -0700

    Added a BYOB dialog for Dataviz. Changed tabs for spaces (didn't mean to do that but I don't want to revert it).

--- assets/metrogram.js
+++ assets/metrogram.js
@@ -1,102 +1,118 @@
 /*
-	Created by Kaspars Dambis
-	http://konstruktors.com
+  Created by Kaspars Dambis
+  http://konstruktors.com
+
+  Modified for CODAME by Felipe Brahm
+  http://codame.com/team
 */
 
+$(function() {
+
+  var $dialog = $('#dialog');
+
+  setInterval(function() {
+    $dialog.slideDown();
+    setTimeout(function() {
+      $dialog.slideUp();
+    }, 10000);
+  }, 20000);
+
+});
+
 var metrogram = angular.module(
-		'metrogram', []
-	).config(
-		['$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ) {
-			$routeProvider.when('/tag/:tag');
-		}]
-  	).controller(
-		'slideshow', function ( $scope, $http, $timeout, $route, $location ) {
-			// Set the API endpoint
-			var api = 'https://api.instagram.com/v1/tags/%tag%/media/recent?access_token=257058201.9af4692.3d68e63b114944a0be332da732923a23&callback=JSON_CALLBACK',
-				newReq, refreshApi;
+    'metrogram', []
+  ).config(
+    ['$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ) {
+      $routeProvider.when('/tag/:tag');
+    }]
+    ).controller(
+    'slideshow', function ( $scope, $http, $timeout, $route, $location ) {
+      // Set the API endpoint
+      var api = 'https://api.instagram.com/v1/tags/%tag%/media/recent?access_token=257058201.9af4692.3d68e63b114944a0be332da732923a23&callback=JSON_CALLBACK',
+        newReq, refreshApi;
 
-			$scope.fetchImages = function() {
-				
-				$scope.loadingClass = 'loading';
-				$scope.imgCurrent = 0;
+      $scope.fetchImages = function() {
 
-				if ( ! $route.current )
-					$location.path( '/tag/' + $scope.tag );
-				else if ( angular.isDefined( $route.current.params.tag ) )
-					$scope.tag = $route.current.params.tag;
+        $scope.loadingClass = 'loading';
+        $scope.imgCurrent = 0;
 
-				$http.jsonp( 
-					api.replace( '%tag%', $scope.tag )
-				).success( function( data ) {
-					delete $scope.loadingClass;
+        if ( ! $route.current )
+          $location.path( '/tag/' + $scope.tag );
+        else if ( angular.isDefined( $route.current.params.tag ) )
+          $scope.tag = $route.current.params.tag;
 
-					$scope.images = data.data;
+        $http.jsonp(
+          api.replace( '%tag%', $scope.tag )
+        ).success( function( data ) {
+          delete $scope.loadingClass;
 
-					// Set the first image active
-					if ( data.data.length )
-						$scope.makeActiveSlide( $scope.imgCurrent );
+          $scope.images = data.data;
 
-					// Cancel the previous update request
-					if ( refreshApi )
-						$timeout.cancel( refreshApi );
+          // Set the first image active
+          if ( data.data.length )
+            $scope.makeActiveSlide( $scope.imgCurrent );
 
-					// Check for new images on every loop
-					if ( data.data.length )
-						refreshApi = $timeout( $scope.fetchImages, 6000 * data.data.length );
-				}).error( function() {
-					delete $scope.loadingClass;
-					refreshApi = $timeout( $scope.fetchImages, 2000 );
-				});
-			}
+          // Cancel the previous update request
+          if ( refreshApi )
+            $timeout.cancel( refreshApi );
 
-			// Fetch images
-			$timeout( $scope.fetchImages );
+          // Check for new images on every loop
+          if ( data.data.length )
+            refreshApi = $timeout( $scope.fetchImages, 6000 * data.data.length );
+        }).error( function() {
+          delete $scope.loadingClass;
+          refreshApi = $timeout( $scope.fetchImages, 2000 );
+        });
+      }
 
-			$scope.advanceSlide = function() {
-				// Method 1
-				// Use a classname to highlight the current active slide
-				if ( angular.isDefined( $scope.images ) && $scope.images.length )
-					$scope.makeActiveSlide( $scope.imgCurrent + 1 );
+      // Fetch images
+      $timeout( $scope.fetchImages );
 
-				/*
-				// Method 2
-				// Just flush the array elements around
-				if ( angular.isDefined( $scope.images ) )
-					$scope.images.push( $scope.images.shift() );
-				*/
+      $scope.advanceSlide = function() {
+        // Method 1
+        // Use a classname to highlight the current active slide
+        if ( angular.isDefined( $scope.images ) && $scope.images.length )
+          $scope.makeActiveSlide( $scope.imgCurrent + 1 );
 
-				$timeout( $scope.advanceSlide, 6000 );
-			}
+        /*
+        // Method 2
+        // Just flush the array elements around
+        if ( angular.isDefined( $scope.images ) )
+          $scope.images.push( $scope.images.shift() );
+        */
 
-			// Advance slides
-			$timeout( $scope.advanceSlide );
+        $timeout( $scope.advanceSlide, 6000 );
+      }
 
-			$scope.makeActiveSlide = function( index ) {
-				// Inactivate the previous slide
-				delete $scope.images[ $scope.imgCurrent ].isActive;
-				// Select the next slide
-				$scope.imgCurrent = ( index ) % $scope.images.length;
-				// Activate the next slide
-				$scope.images[ $scope.imgCurrent ].isActive = true;
-			}
+      // Advance slides
+      $timeout( $scope.advanceSlide );
 
-			$scope.tagChange = function() {
-				$location.path( '/tag/' + $scope.tag );
+      $scope.makeActiveSlide = function( index ) {
+        // Inactivate the previous slide
+        delete $scope.images[ $scope.imgCurrent ].isActive;
+        // Select the next slide
+        $scope.imgCurrent = ( index ) % $scope.images.length;
+        // Activate the next slide
+        $scope.images[ $scope.imgCurrent ].isActive = true;
+      }
 
-				if ( newReq )
-					$timeout.cancel( newReq );
+      $scope.tagChange = function() {
+        $location.path( '/tag/' + $scope.tag );
 
-				newReq = $timeout( function() {
-					$scope.fetchImages();
-					$timeout.cancel( newReq );
-				}, 1000);
-			}
-		}
-	).filter(
-		'escape', function () {
-			return function( input ) {
-				return escape( input );
-			}
-		}	
-	);
+        if ( newReq )
+          $timeout.cancel( newReq );
+
+        newReq = $timeout( function() {
+          $scope.fetchImages();
+          $timeout.cancel( newReq );
+        }, 1000);
+      }
+    }
+  ).filter(
+    'escape', function () {
+      return function( input ) {
+        return escape( input );
+      }
+    }
+  );
 
